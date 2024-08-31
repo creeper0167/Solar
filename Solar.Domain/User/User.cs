facabe656fcad08bbeaac8a83335ba2872920b9e
creeper0167 <m.amin.arjang@gmail.com>
2024-08-31 13:49:22 +0330

    create user model

---
+++ Solar.Domain/User/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Solar.Domain.User;
+
+public class User : BaseEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    public string RefreshToken { get; set; }
+}
