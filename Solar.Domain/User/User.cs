5d61998f5563ece8478b54119259e140bcf73863
Amin <98amiiin@gmail.com>
2024-09-06 12:17:12 +0330

    add class user to project

---
+++ Solar.Domain/User/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solar.Domain.User
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Addres {  get; set; }
+        public string Email { get; set; }
+        public string key1 { get; set; }
+        public string key2 { get; set; }
+        public string Zipcode { get; set; }
+        public int Keymessage { get; set; }
+    }
+}
