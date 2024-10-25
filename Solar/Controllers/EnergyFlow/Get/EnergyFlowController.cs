d0d634cff6b362948b5b4128d375e8a6b7fd7f1a
Amin <98amiiin@gmail.com>
2024-10-25 19:38:43 +0330

    add controller

---
+++ Solar/Controllers/EnergyFlow/Get/EnergyFlowController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Solar.Api.Controllers.EnergyFlow.Get
+{
+    [ApiController]
+    [Route("api")]
+    public class EnergyFlowController : Controller
+    {
+        [HttpGet("EnergyFlow")]
+        public IActionResult Get()
+        {
+            return View();
+        }
+    }
+}
