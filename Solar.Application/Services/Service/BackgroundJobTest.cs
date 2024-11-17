c868f6fcbd5081ae656bc210dd2dae66ed5f23a8
Amin <98amiiin@gmail.com>
2024-11-17 19:52:45 +0330

    Debug login

---
+++ Solar.Application/Services/Service/BackgroundJobTest.cs
@@ -0,0 +1,19 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solar.Application.Services.Service
+{
+    public class BackgroundJobTest : IJob
+    {
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("Task is in progress...");
+            return Task.CompletedTask;
+        }
+    }
+}
